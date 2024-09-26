0eff4bd4a3e63880b0a9f1ae61899ffa4f7a8212
Roman <164523311+Korablinr22@users.noreply.github.com>
2024-09-26 22:10:15 +0700

    Update script.sql

--- Яндекс Практикум/SQL для разработки/script.sql
+++ Яндекс Практикум/SQL для разработки/script.sql
@@ -1,1 +1,13 @@
-select * from table as
+ WITH cte AS (  
+SELECT user_id  
+  FROM stackoverflow.posts  
+ GROUP BY user_id  
+ ORDER BY COUNT(id) DESC  
+ LIMIT 1)   
+
+SELECT EXTRACT(WEEK FROM p.creation_date) AS week,  
+       MAX(p.creation_date) as date  
+  FROM stackoverflow.posts p  
+  JOIN cte ON cte.user_id = p.user_id  
+ WHERE DATE_TRUNC('month', p.creation_date)::date = '2008-10-01'  
+ GROUP BY EXTRACT(WEEK FROM p.creation_date); 
